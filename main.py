b461e6347ce2cd750f7e2bddcc2bb91067a26cf5
Ivan Poplayhin <poplayhin2002@gmail.com>
2022-09-28 20:41:14 +0500

    Пузырек с убыванием

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def bubble(array):
+def bubblevoz(array):
     N = len(array)
     for i in range(N-1):
         for j in range(N-i-1):
@@ -8,7 +8,23 @@ def bubble(array):
                 array[j+1] = buff
     return array
 
+def bubblevub(array):
+    N = len(array)
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if array[j] < array[j+1]:
+                buff = array[j]
+                array[j] = array[j+1]
+                array[j] = buff
+    return array
+
+
+answer = int(input("Введите 1- По убыванию, 2 - по возврастанию.\n"))
+
 array = input("Введите числа через пробел\n")
 array = array.split(" ")
 print(array)
-print(bubble(array))
+if answer == 1:
+    print(bubblevub(array))
+else:
+    print(bubblevoz(array))
